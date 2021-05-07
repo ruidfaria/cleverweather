22a1d656ba105e61e01e69f8963c69ecd47464b5
Rui Faria <ruidfaria@gmail.com>
2021-05-07 10:08:59 +0100

    BUG:clDebug remove kCursor IMP:PWA

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -137,7 +137,12 @@ imports:
 	MatMenuModule			,
 	FlexLayoutModule		,
 	AppRoutingModule		,
-	ServiceWorkerModule.register("/ngsw-worker.js", {enabled: environment.production})
+	ServiceWorkerModule.register('/ngsw-worker.js', {
+		enabled: environment.production,
+		// Register the ServiceWorker as soon as the app is stable
+		// or after 30 seconds (whichever comes first).
+		registrationStrategy: 'registerWhenStable:30000'
+	})
 ],
 providers: 	[],
 bootstrap: 	[
--- deploy.bat
+++ deploy.bat
@@ -1,9 +1,13 @@
 cd Proxy
-rem tsc
-rem call firebase deploy
+cd functions
+call npm install
+call tsc
+cd ..
+call firebase deploy
 
 cd ..
 cd Angular
+call npm install
 call ng build --prod
 call firebase deploy
 
